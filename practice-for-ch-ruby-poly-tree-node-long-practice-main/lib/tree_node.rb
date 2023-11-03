2cedbb3ae522edaa54a93508f60f6250aba5bfd3
ncioffi1 <imnickcioffi@gmail.com>
2023-11-03 13:43:40 -0400

    added first methods

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,25 @@
 class PolyTreeNode
     # testing
+
+    attr_reader :children, :value, :parent
+    # attr_accessor :parent
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        if node != nil
+            @parent = node
+            node.add_child(self)
+        else
+            @parent = nil
+        end
+    end 
+
+    def add_child(node)
+        @children << node
+    end
 end
