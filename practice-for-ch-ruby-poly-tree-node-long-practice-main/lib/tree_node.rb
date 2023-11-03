446102e6ba519784ee8a39bca3e2f31818951de4
csmanel <csmanel@gmail.com>
2023-11-03 14:38:01 -0400

    add parent = functionality

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -10,16 +10,30 @@ class PolyTreeNode
         @children = []
     end
 
-    def parent=(node)
-        if node != nil
-            @parent = node
-            node.add_child(self)
-        else
-            @parent = nil
-        end
+    def parent=(new_parent)
+      @parent.children.delete(self) if @parent != nil  
+
+      if new_parent != nil
+        @parent = new_parent
+        new_parent.children << self
+      else
+          @parent = nil
+      end
     end 
 
     def add_child(node)
         @children << node
+        #itself => node
+        # parent of node = node
+    end
+
+    def remove_child(node)
+
+      if !self.children.include?(node)
+        raise "error"
+      else
+        node.parent = nil
+      end
+
     end
 end
